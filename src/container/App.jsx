baef012b58456baa8bcfbbfa3bf27de3116048e9
Fernando Robles <ferroblesh@gmail.com>
2019-10-10 19:40:30 -0500

    Styles for Search and Search component

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Search from '../components/Search';
 import '../assets/styles/App.scss';
 
-const App = () => {
+const App = () => (
     <div className="App">
         <Header/>
+        <Search/>
     </div>
-}
+)
 
 export default App;
